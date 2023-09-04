7b2f76d7a5e94e84e7bed817be160335be1d8972
reshetovsn <reshetovsn@gmail.com>
2023-09-04 14:56:44 +0300

    Modified User model

--- app/src/main/java/hexlet/code/model/User.java
+++ app/src/main/java/hexlet/code/model/User.java
@@ -1,6 +1,11 @@
 package hexlet.code.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
