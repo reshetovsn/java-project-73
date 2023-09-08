006c4f020628c80986138c94f8fb6147377221c6
reshetovsn <reshetovsn@gmail.com>
2023-09-08 19:40:58 +0300

    try to config auth

---
+++ src/main/java/hexlet/code/config/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package hexlet.code.config.security;
+
+import hexlet.code.service.PersonDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final PersonDetailsService personDetailsService;
+    @Autowired
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
+
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+//        return authConfig.getAuthenticationManager();
+//    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+    // Переопределяем схему аутентификации. Раньше был WebSecurityConfigurerAdapter
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authorize -> authorize
+////                        .requestMatchers("/**").permitAll()
+//                        .anyRequest().permitAll());
+//        return http.build();
+//    }
+
+    // Указываем, что для сравнения хешей паролей будет использоваться кодировщик BCrypt
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/hexlet/code/service/PersonDetailsService.java
@@ -0,0 +1,32 @@
+package hexlet.code.service;
+
+import hexlet.code.config.security.PersonDetails;
+import hexlet.code.model.User;
+import hexlet.code.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PersonDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Autowired
+    public PersonDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(email);
+
+        if (user.isEmpty())
+            throw new UsernameNotFoundException("User not found");
+
+        return new PersonDetails(user.get());
+    }
+}
