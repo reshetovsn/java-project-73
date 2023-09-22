69b3a80eb9c2b5e70ca2791eb9675428227ba993
reshetovsn <reshetovsn@gmail.com>
2023-09-22 18:34:47 +0300

    Modified SecurityConfig

--- src/main/java/hexlet/code/config/security/SecurityConfig.java
+++ src/main/java/hexlet/code/config/security/SecurityConfig.java
@@ -1,44 +1,103 @@
 package hexlet.code.config.security;
 
-import hexlet.code.service.PersonDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
+import hexlet.code.component.JWTHelper;
+import hexlet.code.filter.JWTAuthenticationFilter;
+import hexlet.code.filter.JWTAuthorizationFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.List;
+
+import static hexlet.code.controllers.UserController.USER_CONTROLLER_PATH;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
-    private final PersonDetailsService personDetailsService;
-    @Autowired
-    public SecurityConfig(PersonDetailsService personDetailsService) {
-        this.personDetailsService = personDetailsService;
+
+    public static final String LOGIN = "/login";
+    public static final List<GrantedAuthority> DEFAULT_AUTHORITIES = List.of(new SimpleGrantedAuthority("USER"));
+
+    private final UserDetailsService userDetailsService;
+    private final JWTHelper jwtHelper;
+    private final String baseUrl;
+    private final RequestMatcher loginRequest;
+    private final RequestMatcher publicUrls;
+
+    public SecurityConfig(@Value("${base-url}") final String baseUrl,
+                          final UserDetailsService userDetailsService,
+                          final JWTHelper jwtHelper) {
+        this.baseUrl = baseUrl;
+        this.userDetailsService = userDetailsService;
+        this.jwtHelper = jwtHelper;
+        this.loginRequest = new AntPathRequestMatcher(baseUrl + LOGIN, POST.toString());
+        this.publicUrls = new OrRequestMatcher(
+                loginRequest,
+                new AntPathRequestMatcher(baseUrl + USER_CONTROLLER_PATH, POST.toString()),
+                new AntPathRequestMatcher(baseUrl + USER_CONTROLLER_PATH, GET.toString()),
+                new NegatedRequestMatcher(new AntPathRequestMatcher(baseUrl + "/**"))
+        );
     }
 
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-//        return authConfig.getAuthenticationManager();
-//    }
-
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(personDetailsService);
-    }
-    // Переопределяем схему аутентификации. Раньше был WebSecurityConfigurerAdapter
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(authorize -> authorize
-////                        .requestMatchers("/**").permitAll()
-//                        .anyRequest().permitAll());
-//        return http.build();
-//    }
-
-    // Указываем, что для сравнения хешей паролей будет использоваться кодировщик BCrypt
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers(publicUrls).permitAll()
+                .anyRequest().authenticated().and()
+                .addFilter(new JWTAuthenticationFilter(
+                        authenticationManager(http.getSharedObject(AuthenticationConfiguration.class)),
+                        loginRequest,
+                        jwtHelper
+                ))
+                .addFilterBefore(
+                        new JWTAuthorizationFilter(publicUrls, jwtHelper),
+                        UsernamePasswordAuthenticationFilter.class
+                )
+                .formLogin().disable()
+                .sessionManagement().disable()
+                .logout().disable();
+
+        return http.build();
     }
 }
