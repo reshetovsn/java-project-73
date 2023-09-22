465fe528381151b8f89550381856722290835c54
reshetovsn <reshetovsn@gmail.com>
2023-09-22 18:13:54 +0300

    Modified UserDto

--- src/main/java/hexlet/code/dto/UserDto.java
+++ src/main/java/hexlet/code/dto/UserDto.java
@@ -1,5 +1,9 @@
 package hexlet.code.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +12,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
+
+    @NotBlank(message = "FirstName must not be empty")
     private String firstName;
+
+    @NotBlank(message = "LastName must not be empty")
     private String lastName;
+
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 3, max = 100, message = "Password must be larger than three characters")
+    @JsonIgnore
     private String password;
 }
