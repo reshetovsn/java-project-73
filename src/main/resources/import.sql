b25dc579e226c1ff82dfb7e67f76ca399243dea7
reshetovsn <reshetovsn@gmail.com>
2023-09-28 18:20:04 +0300

    deleted auto dataset

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+-- username: alex@gmail.com password: 12345 $2a$12$lP1HdpYa4yT./wnilPzf0OoxaVHJunEkkxw4SwkYL5obPjq.fG8Xy
+INSERT INTO users (id, first_name, last_name, email, password) VALUES (1, 'Alex', 'Alex', 'alex@gmail.com', '$2a$12$lP1HdpYa4yT./wnilPzf0OoxaVHJunEkkxw4SwkYL5obPjq.fG8Xy');
+
+-- ADMIN
+-- username: admin@gmail.com password: admin_pass
+--INSERT INTO USERS (ID, FIRSTNAME, LASTNAME, EMAIL, PASSWORD) VALUES (2, 'John_admin', 'admin@gmail.com', '$2a$12$rbmC6cJOZqDMXY1oQxupzOliJ2.v9J1EXYiUY2a52qSTpBJVd3vyq', 0);
