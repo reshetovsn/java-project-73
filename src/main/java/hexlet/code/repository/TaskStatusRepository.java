2fce6777c4fb3e3996dd6be8f597abec49d8d40e
reshetovsn <reshetovsn@gmail.com>
2023-09-29 12:00:20 +0300

    Add repository for TaskStatus

---
+++ src/main/java/hexlet/code/repository/TaskStatusRepository.java
@@ -0,0 +1,11 @@
+package hexlet.code.repository;
+
+import hexlet.code.model.TaskStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface TaskStatusRepository extends JpaRepository<TaskStatus, Long> {
+    Optional<TaskStatus> findByName(String name);
+}
