f0da09020ef5d9f7643ffaae0d0e29d56779cdb7
reshetovsn <reshetovsn@gmail.com>
2023-10-02 14:56:11 +0300

    Added TestUtils

---
+++ src/test/java/hexlet/code/utils/TestUtils.java
@@ -0,0 +1,99 @@
+package hexlet.code.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.component.JWTHelper;
+import hexlet.code.dto.TaskStatusDto;
+import hexlet.code.dto.UserDto;
+import hexlet.code.model.User;
+import hexlet.code.repository.TaskStatusRepository;
+import hexlet.code.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.util.Map;
+
+import static hexlet.code.controller.TaskStatusController.TASK_STATUS_CONTROLLER_PATH;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@Component
+public class TestUtils {
+
+    public static final String TEST_USERNAME = "alex@mail.ru";
+    public static final String TEST_USERNAME_2 = "max@mail.ru";
+
+    private final UserDto registrationDto = new UserDto(
+            TEST_USERNAME,
+            "Alex",
+            "Alex",
+            "password");
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
+    @Autowired
+    private JWTHelper jwtHelper;
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+
+    public void tearDown() {
+        taskStatusRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    public User getUserByEmail(final String email) {
+        return userRepository.findByEmail(email).orElseThrow();
+    }
+
+    public ResultActions regDefaultUser() throws Exception {
+        return regUser(registrationDto);
+    }
+
+//    public ResultActions createDefaultTaskStatus() throws Exception {
+//        return createTaskStatus(testDtoForTaskStatus);
+//    }
+
+    public ResultActions regUser(final UserDto userDto) throws Exception {
+        MockHttpServletRequestBuilder request = post("/api/users")
+                .content(MAPPER.writeValueAsString(registrationDto))
+                .contentType(APPLICATION_JSON);
+
+        return perform(request);
+    }
+
+    public ResultActions createTaskStatus(final TaskStatusDto taskStatusDto) throws Exception {
+        final MockHttpServletRequestBuilder request = post(TASK_STATUS_CONTROLLER_PATH)
+                .content(MAPPER.writeValueAsString(taskStatusDto))
+                .contentType(APPLICATION_JSON);
+
+        return perform(request, TEST_USERNAME);
+    }
+
+    public ResultActions perform(final MockHttpServletRequestBuilder request, final String byUser) throws Exception {
+        final String token = jwtHelper.expiring(Map.of("username", byUser));
+        request.header(AUTHORIZATION, token);
+
+        return perform(request);
+    }
+
+    public ResultActions perform(final MockHttpServletRequestBuilder request) throws Exception {
+        return mockMvc.perform(request);
+    }
+
+    public static <T> T fromJson(final String json, final TypeReference<T> to) throws JsonProcessingException {
+        return MAPPER.readValue(json, to);
+    }
+}
