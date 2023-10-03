bab693d64489eb43185c1570332ec6ae274c8854
reshetovsn <reshetovsn@gmail.com>
2023-10-03 22:24:13 +0300

    changes in config files

--- src/test/java/hexlet/code/config/SpringConfigForIT.java
+++ src/test/java/hexlet/code/config/SpringConfigForIT.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.client.RestTemplate;
 
 import static hexlet.code.config.SpringConfigForIT.TEST_PROFILE;
@@ -15,7 +15,7 @@ import static hexlet.code.config.SpringConfigForIT.TEST_PROFILE;
 @Profile(TEST_PROFILE)
 @EnableAspectJAutoProxy
 @ComponentScan(basePackages = "hexlet.code")
-@PropertySource(value = "classpath:/config/application.yml")
+@TestPropertySource(value = "classpath:/config/test-application.yml")
 public class SpringConfigForIT {
 
     public static final String TEST_PROFILE = "test";
