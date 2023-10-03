6df626e760d1dd4ae48165b95481c07996eb324d
reshetovsn <reshetovsn@gmail.com>
2023-10-03 14:48:17 +0300

    modified UserController

--- src/main/java/hexlet/code/controller/UserController.java
+++ src/main/java/hexlet/code/controller/UserController.java
@@ -2,7 +2,6 @@ package hexlet.code.controller;
 
 import hexlet.code.dto.UserDto;
 import hexlet.code.model.User;
-import hexlet.code.repository.UserRepository;
 import hexlet.code.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
